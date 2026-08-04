57d474d0f83e5460d92b7aab60b008764fa7f689
JaneHu2026 <janehu2021168@gmail.com>
2026-08-04 10:26:01 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 知日 Service Worker — 离线可用 + 网络优先更新策略 */
-const CACHE = 'zhiri-v2';
+const CACHE = 'zhiri-v3';
 const PRECACHE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
